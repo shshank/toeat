f7b4a260f3cc0ef1ba79c3d89edea62de1b6982e
adityanayak2010 <nayak.aditya.2010@gmail.com>
2016-03-02 03:10:14 +0530

    Adding test logs

--- resources/main.py
+++ resources/main.py
@@ -1,5 +1,9 @@
 from flask_restful import Resource
+from flask import request
 
 class HelloWorld(Resource):
     def get(self):
+        print request
+        print request.headers
+        print request.data
         return {'hello': 'world'}
